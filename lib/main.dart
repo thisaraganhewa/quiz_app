532f86a42216d6d3d1982e549707f6a87a1636ee
IT21196560 <it21196560@my.sliit.lk>
2024-06-19 15:31:32 +0530

    added new question and added method check answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -31,6 +33,43 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [
+    false, true, true
+  ];
+
+  int questionNumber = 0;
+
+
+  void checkAnswers( bool answer ){
+
+    if( answer == answers[questionNumber] ){
+
+      setState(() {
+        scoreKeeper.add(const Icon(
+          Icons.check,
+          color: Colors.green,
+        ),);
+        ++questionNumber;
+      });
+
+    }
+    else{
+      setState(() {
+        scoreKeeper.add(const Icon(
+          Icons.close,
+          color: Colors.red,
+        ),);
+        ++questionNumber;
+      });
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,15 +77,15 @@ class _QuizPageState extends State<QuizPage> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -75,12 +114,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(const Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ),);
-                });
+                checkAnswers(true);
               },
             ),
           ),
@@ -100,12 +134,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(const Icon(
-                    Icons.close,
-                    color: Colors.red,
-                  ),);
-                });
+                checkAnswers(false);
               },
             ),
           ),
